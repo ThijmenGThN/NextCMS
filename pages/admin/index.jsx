aac98b8e4b8eec244e5203d7b6c74089d91052dd
Thijmen Heuvelink <mail@thijmenheuvelink.nl>
2022-09-05 14:19:21 +0200

    UI Adjustment admin/index

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -20,9 +20,9 @@ export default function Admin() {
 
       <div onKeyPress={({key}) => {if (key == "Enter") authenticate()}} className="mx-auto mt-5 max-w-[350px] bg-white rounded-lg border flex flex-col p-4 gap-1">
         <p className="text-sm text-neutral-500 ml-1">Username</p>
-        <input autoFocus onChange={({target}) => setUsername(target.value)} className={"p-2 border rounded mb-2 hover:bg-neutral-100 " + (error ? 'border-red-500 bg-red-50' : null)} type="text" placeholder="Administrator" />
+        <input autoFocus onChange={({target}) => setUsername(target.value)} className={"p-2 border rounded mb-2 " + (error ? 'border-red-500 bg-red-50 hover:bg-red-100' : 'hover:bg-neutral-100')} type="text" placeholder="Administrator" />
         <p className="text-sm text-neutral-500 ml-1">Password</p>
-        <input onChange={({target}) => setPassword(target.value)} className={"p-2 border rounded mb-2 hover:bg-neutral-100 " + (error ? 'border-red-500 bg-red-50' : null)} type="password" placeholder="••••••••" />
+        <input onChange={({target}) => setPassword(target.value)} className={"p-2 border rounded mb-2 " + (error ? 'border-red-500 bg-red-50 hover:bg-red-100' : 'hover:bg-neutral-100')} type="password" placeholder="••••••••" />
 
         <div className="flex gap-2">
           <button onClick={authenticate} className="bg-cyan-500 py-2 w-full text-white rounded hover:bg-cyan-600">Sign in</button>
